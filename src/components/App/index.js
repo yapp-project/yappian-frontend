0c633b2783b5e22ca5647a1a1067481d8a4e1e0b
Jogeonsang <rhcn258@gmail.com>
2019-03-22 17:33:20 +0900

    Initial setting

---
+++ src/components/App/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./styles.scss";
+import Home from "src/components/Home";
+import Auth from "src/components/Auth";
+const App = props => [
+  // <Navigation key={1} />,
+
+  <PrviateRoutes key={2} />
+
+  // 차후에 사용될 코드이다.
+  // props.isLogin ? <PrviateRoutes key={2} /> : <PublicRoutes key={2} />,
+  // <Footer key={3} />
+];
+
+// isLogin이 True일때 사용되는 Route
+const PrviateRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+  </Switch>
+);
+
+// // isLogin이 Fail일때 사용되는 Route
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Auth} />
+  </Switch>
+);
+export default App;
---
+++ src/components/Auth/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>AUTH</div>;
+  }
+}
+
+export default Auth;
